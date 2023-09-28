3166f1e512db2a40e879bc0af29d101732f84e2f
Maximiliano Fonseca <maximilianofonseca19@gmail.com>
2023-09-28 17:37:12 -0300

    nuevos cambios

--- src/Componentes/Contacto/Contacto.jsx
+++ src/Componentes/Contacto/Contacto.jsx
@@ -16,7 +16,7 @@ export default function Contacto() {
         </div>
       </section>
       <section class="text-gray-400 body-font relative">
-        <div class="container px-5 py-24 mx-auto">
+        <div class="container px-5 py-14 mx-auto">
           <div class="lg:w-1/2 md:w-2/3 mx-auto">
             <div class="flex flex-wrap -m-2">
               <div class="p-2 w-1/2">
@@ -62,14 +62,63 @@ export default function Contacto() {
                   Button
                 </button>
               </div>
-              <div class="p-2 w-full pt-8 mt-8 border-t border-gray-800 text-center">
-                <a class="text-indigo-400">example@email.com</a>
-                <p class="leading-normal my-5">
-                  49 Smith St.
-                  <br />
-                  Saint Cloud, MN 56301
-                </p>
-                <span class="inline-flex">
+              
+            </div>
+          </div>
+        </div>
+      </section>
+      
+
+<section class="text-gray-600 body-font">
+  <div class="container px-5 mx-auto">
+    
+    <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+      <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+        <div class="w-16 h-16 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+        <svg class="h-8 w-8 text-red-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <rect x="3" y="5" width="18" height="14" rx="2" />  <polyline points="3 7 12 13 21 7" /></svg>
+        </div>
+        <div class="flex-grow">
+          <h2 class="text-gray-500 text-lg title-font font-medium mb-3">Email:</h2>
+          <p class="leading-relaxed text-base">maximilianofonseca19@gmail.com</p>
+          
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+        <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+        <svg class="h-8 w-8 text-red-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />  <path d="M15 6l2 2l4 -4" /></svg>
+        </div>
+        <div class="flex-grow">
+          <h2 class="text-gray-500 text-lg title-font font-medium mb-3">Telefono:</h2>
+          <p class="leading-relaxed text-base">+51 3517316163</p>
+         
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
+        <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+            <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+            <circle cx="12" cy="7" r="4"></circle>
+          </svg>
+        </div>
+        <div class="flex-grow">
+          <h2 class="text-gray-500 text-lg title-font font-medium mb-3">Ubicacion:</h2>
+          <p class="leading-relaxed text-base">Cordoba, capita (Argentina)</p>
+          
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</section>
+
+                <section class="text-white body-font relative">
+        <div class="container px-5  mx-auto">
+          <div class="flex flex-col text-center w-full pb-24 mt-6">
+            <h1
+              id="contacto"
+              class="sm:text-4xl text-2xl font-medium title-font text-white"
+            >
+              <span class="inline-flex">
                   <a class="text-gray-500">
                     <svg
                       fill="currentColor"
@@ -128,8 +177,7 @@ export default function Contacto() {
                     </svg>
                   </a>
                 </span>
-              </div>
-            </div>
+            </h1>
           </div>
         </div>
       </section>
